d69871a74a222a62156eff1e8573d572763b2a08
vulkan-ops <vulkanops@gmail.com>
2024-03-17 23:06:01 -0300

    sm8250-common: Remove non-existent modules in PRODUCT_PACKAGES
    build/make/core/main.mk:1342: error: Build failed.
    
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- common.mk
+++ common.mk
@@ -183,13 +183,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     android.hardware.bluetooth@1.1.vendor \
     audio.bluetooth.default \
-    com.qualcomm.qti.bluetooth_audio@1.0.vendor \
     libldacBT_abr \
     libldacBT_enc \
-    libldacBT_dec \
-    libbthost_if \
-    libbthost_if.vendor \
-    libldacBT_bco.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
