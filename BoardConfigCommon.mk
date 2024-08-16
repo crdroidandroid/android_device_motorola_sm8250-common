7534f0e284b8f3f0e4f38f4cf37de52b43b21a6d
Michael Bestas <mkbestas@lineageos.org>
2024-08-16 03:55:37 +0300

    sm8250-common: Bump LOC_HIDL_VERSION to 4.2
    Change-Id: Ic961685f4a1887a2053ff08455ea3c3d0958ac86

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,17 +1,7 @@
 #
 # Copyright (C) 2017-2023 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 BOARD_VENDOR := motorola
@@ -136,7 +126,7 @@ TARGET_FS_CONFIG_GEN := \
 
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
-LOC_HIDL_VERSION := 4.0
+LOC_HIDL_VERSION := 4.2
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
