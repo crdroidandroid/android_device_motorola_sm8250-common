8f20c94738170df565490f028986503077ed2346
vullk4n <vulkanops@gmail.com>
2024-10-18 00:00:27 -0300

    sm8250-common: Use Dolby Atmos from hardware/motorola
    * Reference: https://github.com/userariii/vendor_motorola-dolby.git
    
    Co-authored-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>
    Change-Id: I75366ab88368856e7e3e2d4f2240ee2a450197e4
    Signed-off-by: vullk4n <vulkanops@gmail.com>

--- common.mk
+++ common.mk
@@ -121,6 +121,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.atrace@1.0-service
 
+# Audio (Dolby Atmos)
+$(call inherit-product, hardware/motorola/dolby/setup.mk)
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
