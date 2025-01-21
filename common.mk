dcf2f120652abb52b781ed95ffd95d2cea0e3978
Michael Bestas <mkbestas@lineageos.org>
2025-01-21 07:52:13 +0200

    sm8250-common: Build libloc_socket from source
    Change-Id: I8b9ed4372ba0f3aa14863ae7ce256ad40ea640de

--- common.mk
+++ common.mk
@@ -280,7 +280,8 @@ PRODUCT_PACKAGES += \
     gnss@2.0-xtra-daemon.policy \
     libbatching \
     libgeofencing \
-    libgnss
+    libgnss \
+    libloc_socket
 
 PRODUCT_PACKAGES += \
     apdr.conf \
